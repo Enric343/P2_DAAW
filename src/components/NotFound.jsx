e6bfcbf9521e5657f568f37ee50163e92d620864
Enric343 <enric4343@gmail.com>
2024-12-07 16:29:13 +0100

    First commit

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,10 @@
+
+const notFound = () => {
+    return (
+        <div>
+            <h2>Not Found!</h2>
+        </div>
+    );
+};
+
+export default notFound;
---
+++ src/components/Router.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import App from './App';
+import NotFound from './NotFound';
+
+const Router = () => {
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App/>} />
+                <Route path="*" element={<NotFound/>} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default Router;
